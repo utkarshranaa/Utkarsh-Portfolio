63f5285a341a328bc1656385349da7102a064bb3
Utkarsh Rana <104561101+utkarshranaa@users.noreply.github.com>
2025-03-05 16:44:25 -0500

    added cv and linkedin link in hero-section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,13 +41,13 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://www.linkedin.com/in/utkarshranaa/"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1YeLCPdzN6OvKsSutk9-DpWsItmLFKlvt/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
